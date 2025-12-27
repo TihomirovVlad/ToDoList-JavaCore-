aeca7777ebc26f539af7158cf465cf7360377fab
Vlad Tihomirov <7292963@mail.ru>
2025-12-27 22:23:45 +0300

    add methods (showMenu & choiceMenuItem)

--- src/main/java/ToDoList/UI/Menu.java
+++ src/main/java/ToDoList/UI/Menu.java
@@ -1,4 +1,28 @@
 package ToDoList.UI;
 
+import ToDoList.Repository.TaskRepository;
+
+import java.util.Scanner;
+
 public class Menu {
+    TaskRepository taskRepository = new TaskRepository();
+    public void showMenu(){
+        while(true){
+            System.out.println("1. Add task");
+            System.out.println("2. Remove task by id");
+            System.out.println("3. Show all tasks");
+            System.out.println("4. Show task by id");
+            System.out.println("5. Exit");
+            choiceMenuItem();
+        }
+    }
+
+    public void choiceMenuItem(){
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        switch(choice){
+            case 1:
+        }
+
+    }
 }
