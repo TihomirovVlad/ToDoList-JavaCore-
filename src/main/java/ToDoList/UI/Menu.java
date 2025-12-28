532a46eccad0a8a39b33fafcaf7c36e0ae832311
Vlad Tihomirov <7292963@mail.ru>
2025-12-28 18:47:48 +0300

    add methods for Menu

--- src/main/java/ToDoList/UI/Menu.java
+++ src/main/java/ToDoList/UI/Menu.java
@@ -1,28 +1,89 @@
 package ToDoList.UI;
 
-import ToDoList.Repository.TaskRepository;
+import ToDoList.Model.Priority;
+import ToDoList.Model.Task;
+import ToDoList.Service.Service;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
-    TaskRepository taskRepository = new TaskRepository();
-    public void showMenu(){
-        while(true){
+    Service service = new Service();
+
+    public void showMenu() {
+        while (true) {
             System.out.println("1. Add task");
             System.out.println("2. Remove task by id");
             System.out.println("3. Show all tasks");
             System.out.println("4. Show task by id");
-            System.out.println("5. Exit");
+            System.out.println("5. Show all tasks by priority");
+            System.out.println("6. Exit");
             choiceMenuItem();
         }
     }
 
-    public void choiceMenuItem(){
+    public void choiceMenuItem() {
         Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
-        switch(choice){
+        switch (choice) {
             case 1:
-        }
+                addTask();
+                break;
+            case 2:
+                removeTask();
+                break;
+            case 3:
+                getTasks();
+                break;
+            case 4:
+            {
+                Task taskById = findTaskById();
+                System.out.println(taskById);
+                break;
+            }
+            case 5:
+                Task taskByPriority;
 
+
+        }
+    }
+
+
+    public void addTask() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter task title: ");
+        String title = sc.nextLine();
+        System.out.println("Enter task description: ");
+        String description = sc.nextLine();
+        System.out.println("Enter task priority(Low, Medium, High): ");
+        Priority priority = Priority.valueOf(sc.nextLine());
+        service.addTask(title, description, priority);
+    }
+
+    public void removeTask() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter task id for remove: ");
+        int taskId = sc.nextInt();
+        service.removeTask(taskId);
+    }
+
+    public List<Task> getTasks() {
+        return service.getTasks();
+    }
+
+    public Task findTaskById() {
+        Scanner sc = new Scanner(System.in);
+        int taskId = sc.nextInt();
+        Task task = service.findTaskById(taskId);
+        return task;
+    }
+
+    public List<Task> printTasks() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter task priority(Low, Medium, High): ");
+        Priority priority = Priority.valueOf(sc.nextLine());
+        List<Task> tasksByPriority = service.getTasksByPriority(priority);
+
+        return tasksByPriority;
     }
 }
