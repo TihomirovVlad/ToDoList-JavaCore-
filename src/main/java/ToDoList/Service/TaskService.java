5aa2b61f2017572d9c1650016eebec65bb7873fd
Vlad Tihomirov <7292963@mail.ru>
2025-12-28 18:34:01 +0300

    serviceTask

--- src/main/java/ToDoList/Service/TaskService.java
+++ src/main/java/ToDoList/Service/TaskService.java
@@ -1,6 +1,15 @@
 package ToDoList.Service;
 
+import ToDoList.Model.Priority;
+import ToDoList.Model.Task;
+
+import java.util.List;
+
 public interface TaskService {
 
     public void filterTasks();
+    public void addTask(String title, String description, Priority priority);
+    public void removeTask(int id);
+    public List<Task> getTasks();
+    public Task findTaskById(int id);
 }
