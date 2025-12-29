c87eacf4b21f1f570f2edcaf8641a08a749309a5
Vlad Tihomirov <7292963@mail.ru>
2025-12-29 19:21:59 +0300

    add main

--- src/main/java/ToDoList/Main.java
+++ src/main/java/ToDoList/Main.java
@@ -1,7 +1,10 @@
 package ToDoList;
 
+import ToDoList.UI.Menu;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Menu menu = new Menu();
+        menu.showMenu();
     }
 }
